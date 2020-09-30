a7f1c5b40b32646c40e6fe87d1b8884a1a7bdd0a
EmilyA <emilyadams.3d@gmail.com>
2020-09-30 13:11:31 -0700

    Added Header component to App

--- nb-front-end/src/App.js
+++ nb-front-end/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>New Beginnings</h1>
     </div>
   );
