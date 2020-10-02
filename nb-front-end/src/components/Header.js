6fea0f28f2289ae09655598191c8557fc30e3b21
EmilyA <emilyadams.3d@gmail.com>
2020-10-02 10:10:56 -0700

    Added navbar

--- nb-front-end/src/components/Header.js
+++ nb-front-end/src/components/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
+import NavBar from "./NavBar";
 
 function Header() {
   return (
     <div className="header">
+      <NavBar />
       <h1>Header</h1>
     </div>
   );
---
+++ nb-front-end/src/components/NavBar.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Button,
+} from "reactstrap";
+import logoImg from "../assets/MainLogo.png";
+
+const NavBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="color" light expand="md">
+        <img src={logoImg} style={{ height: "100px" }}></img>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Search</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">About</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                How It Works
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                Logout
+              </NavLink>
+            </NavItem>
+            <Button>Start A New Beginning</Button>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
