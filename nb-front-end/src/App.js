17154ad4e72bc6f66db9dbca68501ff33100fc52
EmilyA <emilyadams.3d@gmail.com>
2020-10-08 19:50:18 -0700

    Fixed some things on the landing page

--- nb-front-end/src/App.js
+++ nb-front-end/src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import mainImg from "./assets/promo4.jpg";
-import { BackgroundImg, colorObj } from "./styles/GlobalStyle";
+import { BackgroundImg } from "./styles/GlobalStyle";
+import styled from "styled-components";
+
+const Centered = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
   return (
     <div className="App">
       <BackgroundImg src={mainImg} alt="" />
       <Header />
+      <Centered>
+        <h1>Discover the future of business and technology</h1>
+      </Centered>
       <div style={{ height: "1200px" }}></div>
-      <h1>Top Beginnings</h1>
+      <h1 style={{ color: "black" }}>Top Beginnings</h1>
       <div
         style={{
           height: "500px",
